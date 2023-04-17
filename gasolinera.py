be3faff214d16cb8df830fd503bacc5050420917
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2023-04-17 17:12:34 +0200

    funcion repostar sin todas las intrucciones

--- gasolinera.py
+++ gasolinera.py
@@ -11,3 +11,22 @@ surtidor = threading.Semaphore(1) #solo un coche puede usar el surtidor a la vez
 disponible = threading.Semaphore(0) #cuando el coche llega al surtidor, y se baja
 acabado = threading.Semaphore(0) #cuando el coche termina de repostar
 mutex = threading.Semaphore(1) #para controlar numero de coches
+
+
+#funcion que simula el repostaje
+def repostar(): #falta añadir tipos combustible y tiempos de repostaje y pago
+    
+    global gasolinera
+    global coches
+    
+    while True:
+        surtidor.acquire()
+        disponible.release()
+        gasolinera = 1
+        print ("Repostando el coche {}".format(coches))
+        time.sleep(1)
+
+        print ("Repostado el coche {}".format(coches))
+        acabado.release()
+        coches -= 1
+        gasolinera = 0
